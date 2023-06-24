5245677e42828aa415dbe5f5bb6b5671b7a3f3a2
Gabrielle Castro <gabriellebcastro@gmail.com>
2023-06-24 15:59:45 -0300

    feat: config book rate and book favorite

--- src/screens/BookLibrary.js
+++ src/screens/BookLibrary.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
-import { getFirestore, doc, deleteDoc } from 'firebase/firestore';
+import { getFirestore, doc, updateDoc } from 'firebase/firestore';
 import { auth } from '../../src/firebase/config.js';
 
 export default function BookLibrary({ route, navigation }) {
   const { book } = route.params;
+  const [rating, setRating] = useState(book.rating || 0);
+  const [isFavorite, setIsFavorite] = useState(book.isFavorite || false);
+
+  const updateBookRating = async (newRating) => {
+    try {
+      const user = auth.currentUser;
+      if (user) {
+        const { uid } = user;
+        const db = getFirestore();
+        const bookRef = doc(db, 'books', book.id);
+        await updateDoc(bookRef, { rating: newRating });
+        setRating(newRating);
+      }
+    } catch (error) {
+      console.log('Error updating book rating:', error);
+    }
+  };
+
+  const toggleFavorite = async () => {
+    try {
+      const user = auth.currentUser;
+      if (user) {
+        const { uid } = user;
+        const db = getFirestore();
+        const bookRef = doc(db, 'books', book.id);
+        await updateDoc(bookRef, { isFavorite: !isFavorite });
+        setIsFavorite(!isFavorite);
+      }
+    } catch (error) {
+      console.log('Error updating favorite status:', error);
+    }
+  };
 
   const removeBookFromLibrary = async () => {
     try {
@@ -13,13 +45,13 @@ export default function BookLibrary({ route, navigation }) {
         const { uid } = user;
         const db = getFirestore();
         const libraryRef = doc(db, 'usuarios', uid, 'library', book.id);
-        console.log('Library reference:', libraryRef); // Verifique se a referência do documento está correta
+        console.log('Library reference:', libraryRef);
         await deleteDoc(libraryRef);
         console.log('Book removed from library');
         navigation.goBack();
       }
     } catch (error) {
-      console.log('Error removing book from library:', error); // Verifique se algum erro é capturado
+      console.log('Error removing book from library:', error);
     }
   };
 
@@ -29,6 +61,31 @@ export default function BookLibrary({ route, navigation }) {
       <Text style={styles.title}>{book.title}</Text>
       <Text style={styles.author}>{book.author}</Text>
       <Text style={styles.pages}>{book.pageCount} pages</Text>
+
+      <View style={styles.ratingContainer}>
+        <TouchableOpacity onPress={() => updateBookRating(1)}>
+          <Text style={[styles.ratingStar, rating >= 1 && styles.ratingStarActive]}>★</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => updateBookRating(2)}>
+          <Text style={[styles.ratingStar, rating >= 2 && styles.ratingStarActive]}>★</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => updateBookRating(3)}>
+          <Text style={[styles.ratingStar, rating >= 3 && styles.ratingStarActive]}>★</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => updateBookRating(4)}>
+          <Text style={[styles.ratingStar, rating >= 4 && styles.ratingStarActive]}>★</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => updateBookRating(5)}>
+          <Text style={[styles.ratingStar, rating === 5 && styles.ratingStarActive]}>★</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.favoriteButton} onPress={toggleFavorite}>
+        <Text style={styles.favoriteButtonText}>
+          {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+        </Text>
+      </TouchableOpacity>
+
       <TouchableOpacity style={styles.removeButton} onPress={removeBookFromLibrary}>
         <Text style={styles.removeButtonText}>Remove from Library</Text>
       </TouchableOpacity>
@@ -60,6 +117,33 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: 'gray',
     marginBottom: 10,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  ratingText: {
+    marginRight: 10,
+    fontWeight: 'bold',
+  },
+  ratingStar: {
+    fontSize: 24,
+    color: 'gray',
+  },
+  ratingStarActive: {
+    color: 'gold',
+  },
+  favoriteButton: {
+    marginTop: 10,
+    backgroundColor: '#151E47',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 8,
+  },
+  favoriteButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
   removeButton: {
     marginTop: 20,
